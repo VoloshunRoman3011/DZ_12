e47968c241a23a1dab8fa92428fec68161df9ab3
Voloshyn.Roman <voloshyn.roman.21@gmail.com>
2020-04-06 14:55:14 +0300

    The End

--- js/scripts.js
+++ js/scripts.js
@@ -97,3 +97,23 @@ hamburger.addEventListener("click", function(e) {
         spanThree.classList.add("spanThreeHide");
     }
 });
+
+$(document).ready(function(){
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        navText: [ '', ' ' ],
+
+        responsive:{
+            0:{
+                items:1
+            },
+
+            1000:{
+                items:1
+            }
+        }
+    });
+});
